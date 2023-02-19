10608cde2c3f0fc5e977ce0e3b90113b16ac00c3
Tobias Grosser <tobias@grosser.es>
2023-02-19 15:02:08 +0000

    Remove val

--- SSASlow.lean
+++ SSASlow.lean
@@ -1,25 +1,22 @@
 inductive Kind where
 | k_a : Kind
 
-structure Val where
-  val: Int
-
 inductive Op where
-| op (name : String) (argval : Val)
+| op (name : String) (argval : Int)
 
 def sem: (o: Op) → Unit
-| .op "a" ⟨_⟩ => ()
-| .op "b" ⟨_⟩ => ()
-| .op "c" ⟨_⟩ => ()
-| .op "d" ⟨_⟩ => ()
-| .op "e" ⟨_⟩ => ()
-| .op "f" ⟨_⟩ => ()
-| .op "g" ⟨_⟩ => ()
-| .op "h" ⟨_⟩ => ()
-| .op "i" ⟨_⟩ => ()
+| .op "a" _ => ()
+| .op "b" _ => ()
+| .op "c" _ => ()
+| .op "d" _ => ()
+| .op "e" _ => ()
+| .op "f" _ => ()
+| .op "g" _ => ()
+| .op "h" _ => ()
+| .op "i" _ => ()
 | _ => ()
 
-theorem Fail: sem (.op "x" ⟨0⟩) = output  := by {
+theorem Fail: sem (.op "x" 0) = output  := by {
   -- tactic 'simp' failed, nested error:
   -- (deterministic) timeout at 'whnf', maximum number of heartbeats (200000) has been reached (use 'set_option maxHeartbeats <num>' to set the limit)
   simp only[sem];
